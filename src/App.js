347bf35cd5c23e65613cbb2f05561df5b02fd247
tsang92 <133945853+tsang92@users.noreply.github.com>
2023-05-20 18:14:56 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ class App {
   constructor(){
     this.name = "WGSConnectd"
     this.version = "1.0.0"
-    this.apiBase = 'http://localhost:3000'
+    //this.apiBase = 'http://localhost:3000'
+    this.apiBase = 'https://courageous-syrniki-e4c28c.netlify.app/'
     this.rootEl = document.getElementById("root")
     this.version = "1.0.0"
   }
